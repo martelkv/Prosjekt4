72b884e661838494fc26eb3ce09964bf35b4ca0e
jelenaj98 <70010699+jelenaj98@users.noreply.github.com>
2020-11-27 16:36:25 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,18 +1,22 @@
-#include "IsingModel.h"
-#include "time.h"
-#include <ctime>
+/* 
+   Program to solve the two-dimensional Ising model 
+   with zero external field using MPI
+   The coupling constant J = 1
+   Boltzmann's constant = 1, temperature has thus dimension energy
+   metropolis sampling is used. Periodic boundary conditions.
+   The code needs an output file on the command line and the variables mcs, nspins,
+   initial temp, final temp and temp step.
+*/
 
+#include "MPIising.h"  
 
+int main(int argc, char** argv){
 
-int main(){
-    std::clock_t c_start = std::clock();
-    IsingModel im(20, 10000, 2.4, 2.4, 1.0); //Matrisedimensjoner, MCcycles, initial temp, final temp, tempstep
-    im.printMatrix();
-    im.run();
-    std::clock_t c_end = std::clock();
-
-    double time_elapsed_ms = 1000.0 * (c_end-c_start) / CLOCKS_PER_SEC;
-    std::cout << "CPU time used: " << time_elapsed_ms /1000.0 << " s\n";
-
+    MPIising mpiising(80,1000000);
+    mpiising.run(argc, argv);
+    
     return 0;
-};
+}
+
+
+
